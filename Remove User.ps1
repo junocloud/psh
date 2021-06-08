eec1937e08768ef89534432f56beee9afdf8b8a8
junocloud <66789283+junocloud@users.noreply.github.com>
2021-06-08 16:53:59 -0300

    Add files via upload

---
+++ Remove User.ps1
@@ -0,0 +1,10 @@
+﻿$acl = Get-Acl "B:\data\backup agir\extrusora\"
+
+$usersid = New-Object System.Security.Principal.Ntaccount ("users")
+
+$acl.PurgeAccessRules($usersid)
+
+$acl | Set-Acl "B:\data\backup agir\extrusora\"
+
+
+#If you put users in usersid ALL users will be removed, use domain\user
