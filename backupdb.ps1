1ef9a1f3ef7be2d72b3ae9e44dfeaa8847b32c26
Anderson Vizoto <66789283+junocloud@users.noreply.github.com>
2022-01-31 14:57:35 -0300

    Add files via upload

---
+++ backupdb.ps1
@@ -0,0 +1,21 @@
+﻿#Run cmd inside powershell
+cmd.exe /c C:\users\azureuser\Desktop\backup.bat
+
+
+#What Folders you want to backup
+$BackupDirs="C:\Users\azureuser\documents\bkp","z:\bkp" 
+
+
+# Delete all Files in $Path older than 30 day(s)
+$Path = $BackupDirs
+$Daysback = "-1"
+ 
+
+#STOP-no changes from here
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
+
+
+#Move Files to Destination
+Copy-Item -Path "c:\users\azureuser\documents\bkp\*" -Destination "Z:\bkp" -Recurse
