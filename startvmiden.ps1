b60d0f0ec2991159eaaa14b322031ff3944e912e
Anderson Vizoto <66789283+junocloud@users.noreply.github.com>
2023-07-28 14:23:45 -0300

    Add files via upload

---
+++ startvmiden.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzAccount -Identity
+$ResourceGroupName = 'ansumantest' 
+$AzureVMName = 'testVM'   
+
+"Starting Azure VM..."
+Stop-AzVM -Name $AzureVMName -ResourceGroupName $ResourceGroupName
